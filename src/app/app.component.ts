a07348a9fd44782c95192878920a0275d42f238c
Anil Mirge <manilche@gmail.com>
2020-12-15 18:43:10 -0500

    Simple Password Generator Angular 11 application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
-
+const num = 7;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pwdGen';
+  password = '';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }  
+  
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }  
+  
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  canGenerate() {
+    return this.length >=5 && (this.includeLetters || this.includeNumbers
+        || this.includeSymbols);
+  }
+
+  onButtonClick() {
+   const numbers = '0123456789';
+   const letters = 'abcdefghijklmnopqrstuvwxyz';
+   const symbols = '!@#$%^&*()';
+
+   let validChars = '';
+   if (this.includeLetters) {
+     validChars += letters;
+   }
+   if (this.includeNumbers) {
+     validChars += numbers;
+   }
+   if (this.includeSymbols) {
+     validChars += symbols;
+   }
+   let generatedPassword = '';
+   for (let i = 0; i < this.length; i++) {
+     const index = Math.floor(Math.random() * validChars.length);
+     generatedPassword += validChars[index];
+   }
+   this.password = generatedPassword;
+  }
 }
